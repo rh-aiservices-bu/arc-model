a02ac53930dbfd809d5f0257d5c4429ea0133df9
Guillaume Moutier <guillaume.moutier@gmail.com>
2022-02-07 12:54:19 -0500

    cValue only, text based

--- prediction.py
+++ prediction.py
@@ -42,7 +42,7 @@ def clean_detections(detections):
                 'xMax': detections['detection_boxes'][i][3]
             },
             'class': detections['detection_class_entities'][i].decode('utf-8'),
-            'cValue': 0.15,
+            'cValue': '15% off',
             'label': detections['detection_class_entities'][i].decode('utf-8'),
             'score': detections['detection_scores'][i],
         }
