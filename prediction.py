07595d77a24bd4688de97194965620733de12e90
Erwan Granger <32554862+erwangranger@users.noreply.github.com>
2022-04-01 15:58:32 -0400

    so you pass the test ith python black

--- prediction.py
+++ prediction.py
@@ -1,28 +1,28 @@
 import tensorflow as tf
 import base64
 
-model_dir = 'models/openimages_v4_ssd_mobilenet_v2_1'
+model_dir = "models/openimages_v4_ssd_mobilenet_v2_1"
 saved_model = tf.saved_model.load(model_dir)
-detector = saved_model.signatures['default']
+detector = saved_model.signatures["default"]
 
 
 def predict(body):
-    base64img = body.get('image')
+    base64img = body.get("image")
     img_bytes = base64.decodebytes(base64img.encode())
     detections = detect(img_bytes)
     cleaned = clean_detections(detections)
 
-    return { 'detections': cleaned }
+    return {"detections": cleaned}
 
 
 def detect(img):
     image = tf.image.decode_jpeg(img, channels=3)
-    converted_img  = tf.image.convert_image_dtype(image, tf.float32)[tf.newaxis, ...]
+    converted_img = tf.image.convert_image_dtype(image, tf.float32)[tf.newaxis, ...]
     result = detector(converted_img)
     num_detections = len(result["detection_scores"])
 
-    output_dict = {key:value.numpy().tolist() for key, value in result.items()}
-    output_dict['num_detections'] = num_detections
+    output_dict = {key: value.numpy().tolist() for key, value in result.items()}
+    output_dict["num_detections"] = num_detections
 
     return output_dict
 
@@ -30,31 +30,32 @@ def detect(img):
 def clean_detections(detections):
     cleaned = []
     max_boxes = 10
-    num_detections = min(detections['num_detections'], max_boxes)
+    num_detections = min(detections["num_detections"], max_boxes)
 
     # hard code coupon value to 0.15 and only include detected class of Clothing
     for i in range(0, num_detections):
         d = {
-            'box': {
-                'yMin': detections['detection_boxes'][i][0],
-                'xMin': detections['detection_boxes'][i][1],
-                'yMax': detections['detection_boxes'][i][2],
-                'xMax': detections['detection_boxes'][i][3]
+            "box": {
+                "yMin": detections["detection_boxes"][i][0],
+                "xMin": detections["detection_boxes"][i][1],
+                "yMax": detections["detection_boxes"][i][2],
+                "xMax": detections["detection_boxes"][i][3],
             },
-            'class': detections['detection_class_entities'][i].decode('utf-8'),
-            'cValue': '15% off',
-            'label': detections['detection_class_entities'][i].decode('utf-8'),
-            'score': detections['detection_scores'][i],
+            "class": detections["detection_class_entities"][i].decode("utf-8"),
+            "cValue": "15% off",
+            "label": detections["detection_class_entities"][i].decode("utf-8"),
+            "score": detections["detection_scores"][i],
         }
-        if (d.get("class") == 'Clothing' and d.get("score") >= 0.15):
+        if d.get("class") == "Clothing" and d.get("score") >= 0.15:
             cleaned.append(d)
 
     return cleaned
 
 
 def preload_model():
-    blank_jpg = tf.io.read_file('blank.jpeg')
+    blank_jpg = tf.io.read_file("blank.jpeg")
     blank_img = tf.image.decode_jpeg(blank_jpg, channels=3)
     detector(tf.image.convert_image_dtype(blank_img, tf.float32)[tf.newaxis, ...])
+
 
 preload_model()
--- wsgi.py
+++ wsgi.py
@@ -5,14 +5,14 @@ from prediction import predict
 application = Flask(__name__)
 
 
-@application.route('/')
-@application.route('/status')
+@application.route("/")
+@application.route("/status")
 def status():
-    return jsonify({'status': 'ok'})
+    return jsonify({"status": "ok"})
 
 
-@application.route('/predictions', methods=['POST'])
+@application.route("/predictions", methods=["POST"])
 def create_prediction():
-    data = request.data or '{}'
+    data = request.data or "{}"
     body = json.loads(data)
     return jsonify(predict(body))
