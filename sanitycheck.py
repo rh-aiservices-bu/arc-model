e76bea505f07a907a88a9d5ff248e4b2b5ed772c
Cameron Garrison <cgarriso@bu.edu>
2022-08-16 09:37:19 -0400

    int parsing, fix #13

--- sanitycheck.py
+++ sanitycheck.py
@@ -36,7 +36,8 @@ def test_detections(detections):
         return False
 
     discount_val = detections[0]["cValue"]
-    discount_val = int(discount_val[:2])
+    discount_val, _, _ = discount_val.partition('%')
+    discount_val = int(discount_val)
 
     # ensure our discount is within our bounds.
     if 0 > discount_val:
